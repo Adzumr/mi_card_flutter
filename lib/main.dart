8fe9dd627f65571ff5382a022777a336c124b742
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2020-05-06 19:00:42 +0100

    Update The Business Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,83 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/Adzumr.jpg'),
+              ),
+              Text(
+                'Umar Abdulaziz Jada',
+                style: GoogleFonts.pacifico(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: GoogleFonts.sourceSansPro(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.teal[200],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 20,
+                  ),
+                  title: Text("adzumrjada@gmail.com"),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 20,
+                  ),
+                  title: Text("+234 813 076 2880"),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
